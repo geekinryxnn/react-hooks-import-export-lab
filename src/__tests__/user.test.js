50d640fa0c7d51af4dfb7dd097f5d5c74447af1d
geekinryxnn <ryankiratu67@gmail.com>
2025-04-10 08:53:00 +0300

    new changes

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,7 +1,7 @@
 import { username, city, image } from "../data/user";
 
 test("username, city, and image are all exported", () => {
-  expect(username).toEqual("Liza");
+  expect(username).toEqual("John Doe");
   expect(city).toEqual("New York");
-  expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
+  expect(image).toEqual("https://via.placeholder.com/150");
 });
